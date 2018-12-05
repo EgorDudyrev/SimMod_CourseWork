2af2962273b4f0bb9db7ff6d877b25eba2121149
EgorDudyrev <egor.dudyrev@yandex.ru>
2018-12-05 09:01:48 +0300

    Add client statuses and small bugfixes

--- SimPy model.py
+++ SimPy model.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[24]:
+# In[1]:
 
 
 import simpy as sp
@@ -34,7 +34,23 @@ cl_columns = ['id','priority','call_start_time','call_end_time','max_waiting_tim
 cl_columns_map = {k:idx for idx,k in enumerate(cl_columns)}
 
 
+# ## Useful functions
+
 # In[4]:
+
+
+def add_client_to_matrix(matrix, priority, call_start_time):
+    data = np.array([-1]*matrix.shape[1])
+    id_ = len(matrix)
+    data[cl_columns_map['id']] = id_
+    data[cl_columns_map['priority']] = priority
+    data[cl_columns_map['call_start_time']] = call_start_time
+    data[cl_columns_map['max_waiting_time']] = 5*60
+    data[cl_columns_map['status']] = map_cl_status_code['generated']
+    return id_, np.append(matrix, [data], axis=0)
+
+
+# In[5]:
 
 
 def get_client_ds(matrix, columns):
@@ -44,18 +60,18 @@ def get_client_ds(matrix, columns):
     client_ds['type'] = client_ds['priority'].transform(lambda x: {1:'gold',2:'silver',3:'regular'}[x])
     client_ds['call_start_time_dt'] = client_ds['call_start_time'].transform(lambda x: dt.timedelta(seconds=x))
     client_ds['call_start_time_dt'] = client_ds['call_start_time_dt'] + dt.datetime(2018,1,1,7)
-    client_ds['call_end_time_dt'] = client_ds['call_end_time'].transform(lambda x: dt.timedelta(seconds=x))
+    client_ds['call_end_time_dt'] = client_ds['call_end_time'].transform(lambda x: dt.timedelta(seconds=x) if x>=0 else None)
     client_ds['call_end_time_dt'] = client_ds['call_end_time_dt'] + dt.datetime(2018,1,1,7)
     client_ds['max_waiting_time_dt'] = client_ds['max_waiting_time'].transform(lambda x: dt.timedelta(seconds=x))
     client_ds = client_ds.reindex(columns=['id','priority','type', 'status_code','status',
-                           'call_start_time','call_start_time_dt','call_end_time', 'call_end_time_end',
+                           'call_start_time','call_start_time_dt','call_end_time', 'call_end_time_dt',
                            'max_waiting_time', 'max_waiting_time_dt'])
     return client_ds
 
 
 # # Testing model
 
-# In[5]:
+# In[6]:
 
 
 class CallCenter(object):
@@ -68,22 +84,29 @@ class CallCenter(object):
     def request_line(self, cl_id):
         cl_priority = self.env.client_mx[cl_id, cl_columns_map['priority']]
         n_lines_to_give = self.n_lines-self.n_vip_lines if cl_priority==3 else self.n_lines
+        Client.set_status_by_id(self.env, cl_id, 'ask_for_line')
         if self.lines.count<n_lines_to_give:
             req = self.lines.request()
+            req.cl_id = cl_id
             yield req
+            Client.set_status_by_id(self.env, cl_id, 'get_line')
         else:
+            Client.set_status_by_id(self.env, cl_id, 'no_lines')
             raise self.NoLinesAvailable()
+        Client.set_status_by_id(self.env, cl_id, 'blocked')
         yield self.env.timeout(8)
+        Client.set_status_by_id(self.env, cl_id, 'unblocked')
         return req
         
     def release_line(self, req):
         yield self.lines.release(req)
+        Client.set_call_end_time_by_id(self.env, req.cl_id)
     
     class NoLinesAvailable(sp.exceptions.SimPyException):
         pass
 
 
-# In[6]:
+# In[7]:
 
 
 class Client(object):
@@ -98,22 +121,24 @@ class Client(object):
             req = yield self.env.process(cc.request_line(self.id_))
         except cc.NoLinesAvailable as e:
             return
+        self.set_status('connected')
         yield self.env.timeout(3)
         yield self.env.process(cc.release_line(req))
-
-
-# In[7]:
-
-
-def add_client_to_matrix(matrix, priority, call_start_time):
-    data = np.array([-1]*matrix.shape[1])
-    id_ = len(matrix)
-    data[cl_columns_map['id']] = id_
-    data[cl_columns_map['priority']] = priority
-    data[cl_columns_map['call_start_time']] = call_start_time
-    data[cl_columns_map['max_waiting_time']] = 5*60
-    data[cl_columns_map['status']] = map_cl_status_code['generated']
-    return id_, np.append(matrix, [data], axis=0)
+        self.set_status('drop_success')
+        
+    @staticmethod
+    def set_status_by_id(env, id_, status):
+        env.client_mx[id_, cl_columns_map['status']] = map_cl_status_code[status]
+    
+    def set_status(self, status):
+        self.set_status_by_id(self.env, self.id_, status)
+        
+    @staticmethod
+    def set_call_end_time_by_id(env, id_):
+        env.client_mx[id_, cl_columns_map['call_end_time']] = env.now
+    
+    def set_call_end_time(self):
+        self.set_call_end_time_by_id(self.env, self.id_)
 
 
 # In[8]:
@@ -127,26 +152,35 @@ def client_generator(env):
         yield env.timeout(1)
 
 
-# In[20]:
+# In[9]:
 
 
-env = sp.Environment()
-env.client_mx = np.empty([0,len(cl_columns)])
-env.call_center = CallCenter(env, 2,0)
-env.client_generator = env.process(client_generator(env))
+def init_env():
+    env = sp.Environment()
+    env.client_mx = np.empty([0,len(cl_columns)])
+    env.call_center = CallCenter(env, 2,0)
+    env.client_generator = env.process(client_generator(env))
+    return env
 
 
-# In[21]:
+# In[10]:
 
 
-for i in tqdm_notebook(range(60*60)):
+env = init_env()
+for i in tqdm_notebook(range(60)):
     env.run(until=i+1)
 
 
-# In[23]:
+# In[11]:
 
 
 client_ds = get_client_ds(env.client_mx, cl_columns)
 print(client_ds.shape)
 client_ds.head()
+
+
+# In[12]:
+
+
+client_ds['status'].value_counts()
 
